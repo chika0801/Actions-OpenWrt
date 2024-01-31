1b0c0fce3fb5f5bc459be49cba91a5c991a96c83
ちか <88967758+chika0801@users.noreply.github.com>
2024-01-31 13:22:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,1 +1,1 @@
-echo 'src-git helloworld https://github.com/fw876/helloworld;master' >> feeds.conf.default
+echo 'src-git helloworld https://github.com/chika0801/helloworld;test' >> feeds.conf.default
